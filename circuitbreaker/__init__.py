27c526fa42a93df7853ae5acd444ff2b2144dd29
BowenDan <bowen.dan@zoro.com>
2016-10-12 21:36:34 -0500

    remove __all__

--- circuitbreaker/__init__.py
+++ circuitbreaker/__init__.py
@@ -1,6 +1,3 @@
 from .circuitbreaker import CircuitBreaker, Endpoint
 from .mock_circuitbreaker import MockCircuitBreaker
 
-__all__ = ['CircuitBreaker',
-           'Endpoint',
-           'MockCircuitBreaker']
