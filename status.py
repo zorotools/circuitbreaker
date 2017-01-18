7818441cee28bb256c6b9cac06a20287c26fd446
Ben DeMeyer <benjamin.demeyer@zoro.com>
2017-01-18 15:11:04 -0600

    Refactored rate checking into a standalone function.

--- status.py
+++ status.py
@@ -6,6 +6,14 @@ from tabulate import tabulate
 from circuitbreaker.circuitbreaker import Endpoint
 
 
+def get_rate(numerator, denominator):
+    if denominator:
+        rate = str(round(numerator / float(len(denominator)) * 100, 2)) + "%"
+    else:
+        rate = "N/A"
+    return rate
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Check the current status of an endpoint')
     parser.add_argument('-H', '--host', dest='host', metavar=('hostname'), default='localhost',
@@ -35,29 +43,21 @@ if __name__ == '__main__':
     if is_healthy:
         history = endpoint.redis.lrange(endpoint.name + ':history', 0, -1)
         successes = history.count(b'1')
-        if history:
-            rate = str(round(successes / float(len(history)) * 100, 2)) + "%"
-        else:
-            rate = "N/A"
         status.extend([
             ('History Cases', len(history)),
             ('History Successes', successes),
-            ('History Success Rate', rate),
+            ('History Success Rate', get_rate(successes, history)),
             ('Failure Threshold', str(endpoint.settings['failure_threshold'] * 100) + "%")
         ])
     else:
         test_count = endpoint.redis.get(endpoint.name + ':test_count')
         test_history = endpoint.redis.lrange(endpoint.name + ':test_history', 0, -1)
         successes = test_history.count(b'1')
-        if test_history:
-            rate = str(round(successes / (float(len(test_history))) * 100, 2)) + "%"
-        else:
-            rate = "N/A"
         status.extend([
             ('Test Count', int(test_count)),
             ('Test History Cases', len(test_history)),
             ('Test History Successes', successes),
-            ('Test History Success Rate', rate),
+            ('Test History Success Rate', get_rate(successes, test_history)),
             ('Recovery Threshold', str(endpoint.settings['recovery_threshold'] * 100) + "%")
         ])
 
